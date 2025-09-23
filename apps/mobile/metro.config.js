0063a9339c935ef0dd18a4d92365e7a964a2d6a6
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-23 10:49:53 +0700

    chore: config RN for monorepo

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,22 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../'),
+  ],
+  resolver: {
+    extraNodeModules: {
+      'react-native': path.resolve(
+        __dirname,
+        '../../node_modules/react-native',
+      ),
+    },
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'],
+    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'webp', 'svg'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
