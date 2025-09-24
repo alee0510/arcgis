42a5d8d676625516d6eb5b6c21d39b1de05e4b3d
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-24 11:14:57 +0700

    chore: implement logout

--- apps/mobile/src/screens/ProfileScreen.tsx
+++ apps/mobile/src/screens/ProfileScreen.tsx
@@ -1,14 +1,16 @@
 import {View, StyleSheet} from 'react-native';
 import {Button, Appbar, MD2Colors} from 'react-native-paper';
+import {useAuth} from '@mobile/store/auth';
 
 function ProfileScreen() {
+  const {clearUser} = useAuth();
   return (
     <View style={styles.container}>
       <Appbar.Header style={styles.header}>
         <Appbar.Content title="Profile" color={MD2Colors.white} />
       </Appbar.Header>
       <View style={styles.content}>
-        <Button mode="contained" onPress={() => {}}>
+        <Button mode="contained" onPress={clearUser}>
           Logout
         </Button>
       </View>
