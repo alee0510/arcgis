4c5b2aa063b1472c264fa74f0e6dc21b0349904b
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-24 17:09:57 +0700

    chore: remove unused location update methods and event listeners from Location class

--- packages/react-native-location/src/index.tsx
+++ packages/react-native-location/src/index.tsx
@@ -1,7 +1,7 @@
+import { NativeModules, Platform } from 'react-native';
 import NativeLocationModule, {
   type LocationResult,
 } from './NativeLocationModule';
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-location' doesn't seem to be linked. Make sure: \n\n` +
@@ -20,10 +20,9 @@ if (!LocationModule) {
   throw new Error(LINKING_ERROR);
 }
 
-const eventEmitter = new NativeEventEmitter(LocationModule);
-
 export type { LocationResult };
 
+// @High-level JS wrapper
 export class Location {
   static async requestLocationPermission(): Promise<boolean> {
     return LocationModule.requestLocationPermission();
@@ -35,26 +34,6 @@ export class Location {
 
   static async getCurrentLocation(): Promise<LocationResult> {
     return LocationModule.getCurrentLocation();
-  }
-
-  static async startLocationUpdates(
-    distanceFilter: number = 10
-  ): Promise<void> {
-    return LocationModule.startLocationUpdates(distanceFilter);
-  }
-
-  static async stopLocationUpdates(): Promise<void> {
-    return LocationModule.stopLocationUpdates();
-  }
-
-  static addLocationListener(callback: (location: LocationResult) => void) {
-    return eventEmitter.addListener('onLocationChanged', callback);
-  }
-
-  static addLocationErrorListener(
-    callback: (error: { code: number; message: string }) => void
-  ) {
-    return eventEmitter.addListener('onLocationError', callback);
   }
 }
 
