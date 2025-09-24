99174558d3159ce0cdc27389e2bab8699c252a29
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-24 10:48:05 +0700

    chore: add config for navigation and tanstack query

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 // @Navigation
 import RootNavigation from '@mobile/navigations/RootNavigation';
 
+// @Tanstack Query
+const queryClient = new QueryClient();
+
 function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <RootNavigation />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
