16886f2d80d536593b1dca5724314771e7a258ad
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-25 21:23:01 +0700

    fix: update event handler types in ArcgisMapViewNativeComponent for improved compatibility

--- packages/react-native-arcgis-map/src/ArcgisMapViewNativeComponent.ts
+++ packages/react-native-arcgis-map/src/ArcgisMapViewNativeComponent.ts
@@ -1,12 +1,11 @@
 import type { HostComponent, ViewProps } from 'react-native';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type { DirectEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
+import type { BubblingEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface NativeProps extends ViewProps {
-  id?: string;
-  // Add any additional props you want to support
-  onReady?: DirectEventHandler<{}>;
-  onError?: DirectEventHandler<{ message: string }>;
+  mapId?: string;
+  onReady?: BubblingEventHandler<{ message: string }>;
+  onError?: BubblingEventHandler<{ message: string }>;
 }
 
 export default codegenNativeComponent<NativeProps>(
