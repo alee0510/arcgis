eb880779f3223cc92fae432cdb3dfdc5f070f8a4
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-26 11:15:45 +0700

    feat: refactor MainScreen to improve map initialization and rendering logic

--- apps/mobile/src/screens/MainScreen.tsx
+++ apps/mobile/src/screens/MainScreen.tsx
@@ -1,15 +1,16 @@
 import {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Text, MD2Colors} from 'react-native-paper';
+import {MD2Colors} from 'react-native-paper';
 import {Location} from '@arcgis/react-native-location';
 import {ArcgisMap, ArcgisMapView} from '@arcgis/react-native-arcgis-map';
+import LoadingScreen from '@mobile/screens/LoadingScreen';
 import type {LocationResult} from '@arcgis/react-native-location';
 import {env} from '@mobile/env';
 
-console.log('ArcgisMap:', ArcgisMap);
-console.log('ArcgisMapView:', ArcgisMapView);
+const MAP_ID = 'e691172';
 export default function MainScreen() {
   const [location, setLocation] = useState<LocationResult | null>(null);
+  const [mapReady, setMapReady] = useState(false);
 
   // Request location permission and get current location
   const requestLocationPermission = async () => {
@@ -19,7 +20,23 @@ export default function MainScreen() {
         const loc = await Location.getCurrentLocation();
         console.log('Current location:', loc);
         setLocation(loc);
-        await ArcgisMap.init(env.ARCGIS_API_KEY);
+        const initResult = await ArcgisMap.init(env.ARCGIS_API_KEY);
+        const createResult = await ArcgisMap.create(
+          MAP_ID,
+          'arcGISTopographic',
+        );
+        const setViewpointResult = await ArcgisMap.setViewpoint(MAP_ID, {
+          latitude: loc.latitude,
+          longitude: loc.longitude,
+          scale: 10000,
+        });
+        console.log(
+          'ArcGIS Map initialized:',
+          initResult,
+          createResult,
+          setViewpointResult,
+        );
+        setMapReady(true);
       } else {
         console.log('Location permission denied');
       }
@@ -30,12 +47,24 @@ export default function MainScreen() {
 
   useEffect(() => {
     requestLocationPermission();
+
+    return () => {
+      ArcgisMap.dispose(MAP_ID);
+    };
   }, []);
 
+  console.log('Rendering MainScreen with location:', location);
   return (
     <View style={styles.container}>
-      <Text variant="headlineMedium">{location?.altitude}</Text>
-      <Text variant="headlineMedium">{location?.longitude}</Text>
+      {mapReady && location ? (
+        <ArcgisMapView
+          mapId={MAP_ID}
+          onError={e => console.error('ArcgisMapView error:', e.nativeEvent)}
+          onReady={e => console.log('ArcgisMapView ready:', e.nativeEvent)}
+        />
+      ) : (
+        <LoadingScreen />
+      )}
     </View>
   );
 }
